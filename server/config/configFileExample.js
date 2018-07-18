fd51253a21a34546c3c9d1beaeb7a5eee2a0ed6e
SergeyTolmachev <stolmachov@gmail.com>
2018-07-18 18:50:51 +0300

    with connect ro DB

---
+++ server/config/configFileExample.js
@@ -0,0 +1,10 @@
+const config = {
+  port: 3000, //порт на котором работает сервер приложения
+  database: 'nameDB', //название базы
+  username: 'username', //пользователь базы данных
+  password: 'password', //пароль базы данных
+  host: 'localhost', //адрес хоста
+  databasePort: 3306, // порт подключения к базе данных
+};
+
+module.exports = config;
